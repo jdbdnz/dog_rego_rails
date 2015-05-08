25544736e0e3580fd237d28f755e0498cc7c2156
Josh Dean <jdbdean@gmail.com>
2015-05-08 15:11:26 +1200

    will use google's geocoder API to resolve address, so we store hash (parsed JSON) of they return then use wrapper method for the common use case of viewing postal address

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  serialize :postal_address, JSON
+  serialize :postal_address
 
   has_many :dogs
+  has_many :registrations, through: :dogs
+
+  def postal_address_formatted
+    postal_address['results'][0]['formatted_address']
+  end
+
 end
