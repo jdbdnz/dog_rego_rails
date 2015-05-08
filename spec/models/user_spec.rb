9363ee876100932fcc1d467866ab52620d58cfb0
Josh Dean <jdbdean@gmail.com>
2015-05-08 15:14:02 +1200

    test to ensure that the address wrapper for the serialised geocoded information returns string

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) { FactoryGirl.create(:user) }
+
+  # TODO: split this into a few more specific tests so we don't have to depend on the factory not changing
+  it "should return formatted address string from postal_address json" do
+    expect( user.postal_address_formatted ).to eq "100 Twin Peaks Highway, Twin Peaks, WA 98203, USA"
+  end
+
 end
