394c310e2609c583c20d02c4f3184739784dbf65
Josh Dean <jdbdean@gmail.com>
2015-05-09 14:36:32 +1200

    registration queries

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,4 +5,13 @@ class Dog < ActiveRecord::Base
   def age
     Date.current.year - self.date_of_birth.year
   end
+
+  def current_registration
+    registrations.current.first
+  end
+
+  def current_registration?
+    current_registration.present?
+  end
+  
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,10 @@
 class Registration < ActiveRecord::Base
+  
   belongs_to :dog
-  delegate :name, :breed, :date_of_birth, to: :dog
+  delegate :owner, :name, :breed, :date_of_birth, to: :dog
+
+  REGISTRATION_PERIOD_PRICES = { 3 => 25, 6 => 65, 12 => 85 }
+
+  scope :current, -> { where 'valid_from < ? AND valid_till > ?', DateTime.current, DateTime.current }
+
 end
