6d2c43e91b55a46ca6eb35fa780ea33c2e300688
Josh Dean <jdbdean@gmail.com>
2015-05-09 14:35:01 +1200

    allow compatability for non geocoded addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,17 @@ class User < ActiveRecord::Base
   has_many :registrations, through: :dogs
 
   def postal_address_formatted
-    postal_address['results'][0]['formatted_address']
+    if postal_address.is_a? String
+      # user continued without selecting a geocoded suggestion
+      # good to have this ability in case the address from Google isn't correct
+      postal_address
+    else
+      begin
+        postal_address['results'][0]['formatted_address']
+      rescue
+        nil
+      end
+    end
   end
 
 end
