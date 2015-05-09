c0f709cb3d58f2da93f37f193b8eb8953ce52fb3
Josh Dean <jdbdean@gmail.com>
2015-05-09 12:09:56 +1200

    basic page anatomy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+
+//= require bootstrap
+//= require bootstrap-sprockets
+//= require modernizr
+
 //= require_tree .
+
+//= require turbolinks
+
+
